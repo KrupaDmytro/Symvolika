8416911aae85b442392be653dd2a7cfb10bd535d
KrupaDmytro <dima-krupa@ukr.net>
2016-05-19 19:43:12 +0300

    Add files via upload

--- upload.php
+++ upload.php
@@ -50,11 +50,6 @@
         <header class="w3-container w3-card-4 w3-theme">
 		    <title>Символіка</title>
 			<h1>Магазин військової атрибутики - Символіка</h1>
-				<center>
-					<span id="registration"></span>
-					<button id="login" class="w3-btn w3-theme-l3">Увійти</button>				
-					<p id="log"><p>
-				</center>
         </header>
 			
 			<div class="w3-light-aqua w3-container w3-padding-32 w3-center">
@@ -66,61 +61,6 @@
 			<li><a href="store.html">Магазин</a></li>
 			<li><a href="about.html">Про</a></li>
 		</ul>
-		
-		<!-- The Modal Dialog for authorization -->
-        <form id="logindialog" class="w3-modal">
-            <div class="w3-modal-content w3-animate-top w3-card-8">
-                <header class="w3-container w3-green">
-						<span onclick="document.getElementById('logindialog').style.display='none'"
-                        class="w3-closebtn">&times;</span>
-                    <p>Будь ласка, введіть свої облікові дані нижче:</p>
-                </header>
-				<div class="w3-container">
-                    <br><label class="w3-label">ЕЛ. ПОШТА</label>
-                    <input class="w3-input" type="email" id="log_email" required>
-                     
-                    <br><label class="w3-label">ПАРОЛЬ</label>
-                    <input class="w3-input" type="password" id="log_pass" required>
-                    
-                    <p><button id="loginbutton" class="w3-btn w3-theme-l3">УВІЙТИ</button>
-                    
-                    <p>Ще не зареєстровані?
-                    <button id="showregisterdialog" class="w3-btn w3-theme-l3">ЗАРЕЄСТРУВАТИСЯ</button>      
-                </div>
-            </div>
-        </form>
-		
-		<!-- The Modal Dialog for registration -->
-        <form id="registerdialog" class="w3-modal">
-            <div class="w3-modal-content w3-animate-top w3-card-8">
-                <header class="w3-container w3-green">
-						<span onclick="document.getElementById('registerdialog').style.display='none'"
-                        class="w3-closebtn">&times;</span>
-                    
-                    <p>Будь ласка, надайте таку інформацію для реєстрації:</p>
-                </header>
-					<div class="w3-container">
-                    <br><label class="w3-label">ІМ'Я*</label>
-                    <input class="w3-input" type="text" id="reg_first" required>
-    
-                    <label class="w3-label">ПРІЗВИЩЕ*</label>
-                    <input class="w3-input" type="text" id="reg_last" required>
-                        
-                    <label class="w3-label">ЕЛЕКТРОННА ПОШТА*</label>
-                    <input class="w3-input" type="email" id="reg_email" required>
-                     
-                    <label class="w3-label">ПАРОЛЬ*</label>
-                    <input class="w3-input" type="password" id="reg_pass" required>
-                     
-                    <label class="w3-label">ПІДТВЕРДЬТЕ ПАРОЛЬ*</label>
-                    <input class="w3-input" type="password" id="reg_verpass" required>
-                        
-                    <p><button id="register" class="w3-btn w3-theme-l3 w3-btn w3-btn-block w3-section">ЗАРЕЄСТРУВАТИСЯ</button>
-                        
-                    </table>
-                </div>
-            </div>
-        </form>
 			
 
         <div class="w3-card-4 w3-margin">
